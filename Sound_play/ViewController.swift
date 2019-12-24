6be30feabcc20ef6b4e3d3d09b14761c8b2f54bd
Mesba <mesbaulhasanbsmrstu@gmail.com>
2019-12-25 00:47:44 +0600

    Initial Commit

---
+++ Sound_play/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sound_play
+//
+//  Created by M29 on 25/12/19.
+//  Copyright © 2019 M29. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sound_playTests/Sound_playTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sound_playTests.swift
+//  Sound_playTests
+//
+//  Created by M29 on 25/12/19.
+//  Copyright © 2019 M29. All rights reserved.
+//
+
+import XCTest
+@testable import Sound_play
+
+class Sound_playTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
