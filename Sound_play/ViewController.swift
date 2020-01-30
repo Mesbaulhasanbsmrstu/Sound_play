625a1bb42e379a78fa39e0f3630c6dc77549c88e
Mesba <mesbaulhasanbsmrstu@gmail.com>
2020-01-31 00:58:49 +0600

    update

--- Sound_play/ViewController.swift
+++ Sound_play/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+let soundarray = ["red","orange","yellow","green","indigo","blue","purple"]
+    var player : AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func okay(_ sender: UIButton) {
+        playsound(sound: soundarray[sender.tag])
+        
+    }
+    func playsound(sound: String)
+    {
+        let soundURL = Bundle.main.url(forResource: sound , withExtension: "wav")
+        do
+        {
+            player = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch{
+            print(error)
+        }
+        player.play()
+    }
 }
 
